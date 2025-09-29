fb07f5b9494635d3451cecc21eb4d453e2c55a9f
M4zek <mazur13aa@gmail.com>
2025-09-30 00:28:58 +0200

    Component creation - Creation of a spinner to display company portfolios

--- frontend/src/app/pages/company-view/company-view.component.ts
+++ frontend/src/app/pages/company-view/company-view.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ImageListComponent} from "../../components/image-list/image-list.component";
 
 @Component({
   selector: 'app-company-view',
-  imports: [],
+  imports: [
+    ImageListComponent
+  ],
   templateUrl: './company-view.component.html',
   styleUrl: './company-view.component.css'
 })
 export class CompanyViewComponent {
+
+  companyName: string = 'Default company name';
+  companyAddress: string = 'Street 121, New York';
+
+
+
+  likeCompany(){
+    console.log("Liked company")
+  }
+
+  shareCompany(){
+    console.log("Shared company")
+  }
 
 }
