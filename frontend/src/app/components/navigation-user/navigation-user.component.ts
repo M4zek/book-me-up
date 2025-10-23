5488c0fe86ab8241722d2ee65c205ccd7ef8a50d
M4zek <mazur13aa@gmail.com>
2025-10-23 15:19:55 +0200

    Minor changes to top bars and user navigation bar
    - Added routing for user navigation bar
    - Minor visual changes

--- frontend/src/app/components/navigation-user/navigation-user.component.ts
+++ frontend/src/app/components/navigation-user/navigation-user.component.ts
@@ -1,11 +1,21 @@
 import {Component, HostListener} from '@angular/core';
-import {NgClass, NgIf} from "@angular/common";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {Router, RouterLink} from "@angular/router";
+
+
+export interface MainNavigation{
+  name: string;
+  icon?: string;
+  path: string;
+}
 
 @Component({
   selector: 'app-navigation-user',
   imports: [
     NgIf,
-    NgClass
+    NgClass,
+    NgForOf,
+    RouterLink
   ],
   templateUrl: './navigation-user.component.html',
   styleUrl: './navigation-user.component.css'
@@ -14,18 +24,49 @@ export class NavigationUserComponent {
   user_avatar: string = 'images/user_default_avatar.png';
   user_name: string = 'John Doe';
   drop_down_icon: string = 'icons/drop_down_arrow.png';
-  search_icon: string = 'icons/search_icon.svg';
-  localization_icon: string = 'icons/localization_icon.svg';
-  settings_icon: string = 'icons/settings_icon.svg';
-  messages_icon: string = 'icons/message_icon.svg';
-  company_icon: string = 'icons/company_icon.svg';
-  calendar_icon: string = 'icons/calendar_stroke_icon.svg';
-  home_icon: string = 'icons/home_icon.svg';
   confirm_icon: string = 'icons/confirm_icon.svg';
   reject_icon: string = 'icons/reject_icon.svg';
 
   menuOpen: boolean = false;
   isActive = false;
+
+  constructor(public router: Router) {}
+
+  BAR_NAVIGATION: MainNavigation[] = [
+    {
+      name: 'Home',
+      icon: 'icons/home_icon.svg',
+      path: '/app/home/welcome',
+    }
+  ]
+
+  NAVIGATION_USER: MainNavigation[] = [
+      {
+        name: 'My appointments',
+        icon: 'icons/calendar_stroke_icon.svg',
+        path: '/app/appointments',
+      },
+      {
+        name: 'My companies',
+        icon: 'icons/company_icon.svg',
+        path: '/app/company-management',
+      },
+      {
+        name: 'Chat',
+        icon: 'icons/message_icon.svg',
+        path: '/app/messages',
+      },
+      {
+        name: 'Settings',
+        icon: 'icons/settings_icon.svg',
+        path: '/app/settings',
+      },
+      {
+        name: 'Logout',
+        icon: 'icons/logout_icon.svg',
+        path: '/guest',
+      }
+  ]
 
   toggleSearch() {
     this.isActive = !this.isActive;
