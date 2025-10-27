14f74c297947771dbcfaa9e0f9198c113b19d0b6
M4zek <mazur13aa@gmail.com>
2025-10-27 19:12:03 +0100

    Creation of a preliminary modal for adding a new company
    - Change of the navigation bar in company management (addition of a button to open the add modal)
    - Creation of a component with a modal

--- frontend/src/app/pages/company-management-page/company-management-navigation/company-management-navigation.component.ts
+++ frontend/src/app/pages/company-management-page/company-management-navigation/company-management-navigation.component.ts
@@ -2,6 +2,9 @@ import {Component} from '@angular/core';
 import {DropDownListComponent} from "../../../components/drop-down-list/drop-down-list.component";
 import {NgClass, NgForOf} from "@angular/common";
 import {Router, RouterLink} from "@angular/router";
+import {
+  CompanyManagementAddCompanyModalComponent
+} from "../../../components/modals/company-management-add-company-modal/company-management-add-company-modal.component";
 
 export interface NavCompanyManagementType {
   name: string;
@@ -18,12 +21,15 @@ export interface CompanyManagementNavigation {
     DropDownListComponent,
     NgForOf,
     NgClass,
-    RouterLink
+    RouterLink,
+    CompanyManagementAddCompanyModalComponent
   ],
   templateUrl: './company-management-navigation.component.html',
   styleUrl: './company-management-navigation.component.css'
 })
 export class CompanyManagementNavigationComponent {
+
+  isOpenAddDialog: boolean = false;
 
   NAVIGATION: CompanyManagementNavigation = {
     homePage: {name: 'Home', path: '/app/company-management/home'},
@@ -38,5 +44,13 @@ export class CompanyManagementNavigationComponent {
   constructor(public router: Router) {
   }
 
+
+  openAddCompanyDialog() {
+    this.isOpenAddDialog = true;
+  }
+
+  closeAddCompanyDialog() {
+    this.isOpenAddDialog = false;
+  }
 
 }
