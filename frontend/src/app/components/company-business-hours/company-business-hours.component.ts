fbef4929adf6e71f412281927593ebbe08a7b88a
M4zek <mazur13aa@gmail.com>
2025-11-11 13:11:33 +0100

    Minor changes to the appearance for displaying company opening hours and company employees

--- frontend/src/app/components/company-business-hours/company-business-hours.component.ts
+++ frontend/src/app/components/company-business-hours/company-business-hours.component.ts
@@ -15,4 +15,9 @@ import {CompanyHours} from "../../model/response/company-response.model";
 export class CompanyBusinessHoursComponent {
   @Input() openingHours: CompanyHours[] = []
 
+
+    isToday(dayOfWeek: string) {
+        const todayName = new Date().toLocaleDateString("en-US", {weekday: "long"});
+        return dayOfWeek == todayName;
+    }
 }
